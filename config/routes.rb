5bdf858ec4bc1b4c361b6d1c4504ac0335a808a7
austinb847 <austinb847@icloud.com>
2020-07-25 12:53:51 -0700

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       post '/login', to: 'sessions#create'
       delete '/logout', to: 'sessions#destroy'
       get '/logged_in', to: 'sessions#is_logged_in?'
+      get '/admin_logged_in', to: 'sessions#is_admin?'
       resources :users
       resources :pedals
       get '/random' => 'pedals#random'
